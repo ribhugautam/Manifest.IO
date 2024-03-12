c7f376aac4ac972c35f2a6adcb963f5a609ff300
ribhugautam <gautamribhu@gmail.com>
2024-03-12 12:20:41 +0530

    cg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import { account } from '../appwrite/config';
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-full w-11/12 max-w-[1180px] mx-auto ">
+    <div className='bg-slate-100 flex flex-col items-center justify-center h-full'>
+    <div className="flex flex-col items-center justify-center w-11/12 max-w-[1180px] mx-auto ">
       {/* Hero Section */}
       <section className="hero w-full max-w-4xl text-center">
         <h1 className="text-4xl font-bold mb-2">Welcome to Manifest.io</h1>
-        
       </section>
+    </div>
     </div>
   );
 }
