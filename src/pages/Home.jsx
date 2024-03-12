268eed200e2153c5639c6ce8c553c9c523914482
ribhugautam <gautamribhu@gmail.com>
2024-03-12 12:17:57 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react';
 import { account } from '../appwrite/config';
 
 function Home() {
-
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col items-center justify-center h-full w-11/12 max-w-[1180px] mx-auto ">
+      {/* Hero Section */}
+      <section className="hero w-full max-w-4xl text-center">
+        <h1 className="text-4xl font-bold mb-2">Welcome to Manifest.io</h1>
+        
+      </section>
+    </div>
+  );
 }
 
 export default Home
